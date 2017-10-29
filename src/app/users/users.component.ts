284291a736815e9e339c18539d92072ccd179eb8
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-29 17:08:25 +0100

    add users component

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-users',
+    template: `
+        <p>I'm in the users component</p>
+    `
+})
+export class UsersComponent {
+
+}
