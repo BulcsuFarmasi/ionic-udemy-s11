1f3ebb1128677052b58acb4e4ecfee0da010cf9f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-29 17:53:23 +0100

    directives

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
     selector: 'app-users',
     template: `
         <p>I'm in the users component</p>
+        <button (click)="showInfo = true">Show info</button>
+        <p *ngIf="showInfo">Some extra user information!</p>
+        <p *ngFor="let user of users">{{ user }}</p>
+        <p [ngStyle]="{'background-color': color}">This is green!</p>
     `
 })
 export class UsersComponent {
-
+    showInfo = false;
+    color = 'green';
+    users = ['Bulcsú', 'Dorka', 'Csenge']
 }
